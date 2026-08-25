f2447209437606cf31cb2097ca5bc6b890a85a59
devika <devikah.7b@gmail.com>
2026-08-25 14:08:52 +0700

    fix

--- back-vex/app/Models/Pameran.php
+++ back-vex/app/Models/Pameran.php
@@ -48,15 +48,12 @@ class Pameran extends Model
         return $this->belongsTo(ModelPameran::class, 'model_pameran', 'id_model');
     }
 
-<<<<<<< HEAD
-=======
     // Relasi ke tabel kategori
     public function kategori()
     {
         return $this->belongsTo(Kategori::class, 'kategori_kode', 'kode_kategori');
     }
 
->>>>>>> creator-category
     public function karya()
     {
         return $this->hasMany(Karya::class, 'id_pameran', 'id_pameran');
