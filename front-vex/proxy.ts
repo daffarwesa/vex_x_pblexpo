5dfb98caf99108f9a0c9d7ef546f20a69da64177
fajpras <fajrinurpras07@gmail.com>
2026-08-26 23:22:05 +0700

    Edit proxy for testing UI only

--- front-vex/proxy.ts
+++ front-vex/proxy.ts
@@ -1,32 +1,32 @@
 import { NextResponse } from 'next/server';
 import type { NextRequest } from 'next/server';
 
-export function proxy(request: NextRequest) {
-  const role = request.cookies.get('role')?.value;
-  const pathname = request.nextUrl.pathname;
+export function proxy(request: NextRequest) {}
+//   const role = request.cookies.get('role')?.value;
+//   const pathname = request.nextUrl.pathname;
 
-  const isAdminPage = pathname.startsWith('/admin');
-  const isCreatorPage = pathname.startsWith('/creator');
+//   const isAdminPage = pathname.startsWith('/admin');
+//   const isCreatorPage = pathname.startsWith('/creator');
 
-  if (!isAdminPage && !isCreatorPage) {
-    return NextResponse.next();
-  }
+//   if (!isAdminPage && !isCreatorPage) {
+//     return NextResponse.next();
+//   }
 
-  if (!role) {
-    return NextResponse.redirect(new URL('/login', request.url));
-  }
+//   if (!role) {
+//     return NextResponse.redirect(new URL('/login', request.url));
+//   }
 
-  if (isAdminPage && role !== 'Admin') {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
+//   if (isAdminPage && role !== 'Admin') {
+//     return NextResponse.redirect(new URL('/', request.url));
+//   }
 
-  if (isCreatorPage && role !== 'Creator') {
-    return NextResponse.redirect(new URL('/', request.url));
-  }
+//   if (isCreatorPage && role !== 'Creator') {
+//     return NextResponse.redirect(new URL('/', request.url));
+//   }
 
-  return NextResponse.next();
-}
+//   return NextResponse.next();
+// }
 
-export const config = {
-  matcher: ['/admin/:path*', '/creator/:path*'],
-};
+// export const config = {
+//   matcher: ['/admin/:path*', '/creator/:path*'],
+// };
